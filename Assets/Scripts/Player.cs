34e0f0d53f1d8b3e1b65d1bcf61bc1f95f6cf411
Astranomikon <116378098+Astranomikon@users.noreply.github.com>
2023-02-16 15:01:30 +0500

    Change Player, SliderValue. Delete HealthChange

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class Player : MonoBehaviour
@@ -9,19 +10,26 @@ public class Player : MonoBehaviour
     public float MaxHealth { get; private set; }
     public float MinHealth { get; private set; }
 
+    public event UnityAction Changed;
+
+    private float _damageValue = 10;
+    private float _healthValue = 10;
+
     private void Start()
     {
         Health = 50;
         MaxHealth = 100;
         MinHealth = 0;
     }
-    public void TakeDamage(float damage)
+    public void TakeDamage()
     {
-        Health = Mathf.Max(MinHealth, Health - damage);    
+        Health = Mathf.Max(MinHealth, Health - _damageValue);
+        Changed?.Invoke();
     }
 
-    public void TakeHeal(float heal)
+    public void TakeHeal()
     {
-        Health = Mathf.Min(MaxHealth, Health + heal);
+        Health = Mathf.Min(MaxHealth, Health + _healthValue);
+        Changed?.Invoke();
     }
 }
--- Assets/Scripts/SliderValue.cs
+++ Assets/Scripts/SliderValue.cs
@@ -3,46 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(HealthChange))]
 
 public class SliderValue : MonoBehaviour
 {
     [SerializeField] private Slider _slider;
+    [SerializeField] private Player _player;
 
-    private HealthChange healthChange;
     private float _deltaValue = 10;
     private Coroutine _coroutine;
-    private float _targetValue;
 
     private void Start()
     {
-        healthChange = GetComponent<HealthChange>();
-    }
-
-    public void SetTargetValue(float value)
-    {
-        _targetValue = value;
-        healthChange.Changed += NewValue;
+        _player.Changed += NewValue;
     }
 
     private void NewValue()
     {
-        Debug.Log(_targetValue);
+        Debug.Log(_player.Health);
 
         if (_coroutine != null)
-        {
             StopCoroutine(_coroutine);
-            healthChange.Changed -= NewValue;
-        }
 
         _coroutine = StartCoroutine(ChangeValue());
     }
 
     private IEnumerator ChangeValue()
     {
-        while(_targetValue != _slider.value)
+        while(_player.Health != _slider.value)
         {
-            _slider.value = Mathf.MoveTowards(_slider.value, _targetValue, _deltaValue * Time.deltaTime);
+            _slider.value = Mathf.MoveTowards(_slider.value, _player.Health, _deltaValue * Time.deltaTime);
             yield return null;
         }
     }
